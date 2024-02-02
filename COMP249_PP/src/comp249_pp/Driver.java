0f9285a12435cd1715f8c1caa00bb8484a367f80
abdel-concordia <abdel@MSSPro.wireless.concordia.ca>
2024-02-01 19:41:18 -0500

    While working on the class student

--- COMP249_PP/src/comp249_pp/Driver.java
+++ COMP249_PP/src/comp249_pp/Driver.java
@@ -1,23 +1,50 @@
 package comp249_pp;
 
-public class Driver {
+public class Driver extends Object {
 
     public static void main(String[] args) {
 
-        Person p1 = new Person("John", 20, "Montreal");
-        Person p2 = new Person("Jane", 19);
-        Person p3 = new Person(17);
-        Person p4 = new Person(); // Default constructor
+        Address ad = new Address(1234, "Ste-Catherine", "H3", "Montreal");
 
-        System.out.println(p1);
-        f(p1);
-        System.out.println(p1);
+        Person p1 = new Person("John", 20, ad);
 
+        Person p2 = new Person("Jane", 19, new Address(5678, "Guy", "H3", "Montreal"));
+
+        Person p3 = new Person("Jane", 19, 5678, "Guy", "H3", "Montreal");
+
+        Person p4 = new Person(p1);
+
+        Address ad2 = p1.getAddress();
+
+        System.out.println(ad2);
+
+        ad2.setCityName("Sherbrooke");
+        System.out.println(p1.getAddress().getCityName());
+
+        //p4.setName("New Name");
+        //System.out.println(p1.getName());
+        //System.out.println(p4.getName());
+        //p4.getAddress().setCityName("Laval");
+        //System.out.println(p1.getAddress().getCityName());
+        //System.out.println(p4.getAddress().getCityName());
+        //Person p3 = new Person(17);
+        //Person p4 = new Person(); // Default constructor
+        //System.out.println(p1);
+        //f(p1);
+        //System.out.println(p1);
+        //int a = 5;
+        //g(a);
+        //System.out.println("After calling g: " + a);
+    }
+
+    public static void g(int x) {
+        x = x + 5;
+        System.out.println("In method g: " + x);
     }
 
     public static void f(Person p) {
         //p.setName("New name");
-        p = new Person("New name", 60, "Laval");
+        //p = new Person("New name", 60, "Laval");
     }
 
 }
